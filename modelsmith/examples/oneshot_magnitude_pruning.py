050282cc9ac1dbb8045e327d7579e837b9e2056d
Pop John <john.pop@airgap.io>
2024-02-26 10:23:17 +0200

    fix pruning scripts

--- modelsmith/examples/oneshot_magnitude_pruning.py
+++ modelsmith/examples/oneshot_magnitude_pruning.py
@@ -26,104 +26,107 @@ from models import *
 from utils.utils import progress_bar, train, test
 from utils.pruner import pruning_model_random, check_sparsity, pruning_model, prune_model_custom, extract_mask, remove_prune
 
-# In[2]: Argument parser
-parser = argparse.ArgumentParser(description='Post-training pruning: iterative magnitude pruning (IMP)')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--epochs', default=2, type=int, help='number of epochs for each training period')
-parser.add_argument('--device', default='cuda' if torch.cuda.is_available() else 'cpu', type=str, help='device to use')
-parser.add_argument('--best_acc', default=0, type=float, help='best test accuracy')
-parser.add_argument('--start_epoch', default=0, type=int, help='start epoch')
-parser.add_argument('--pruning_times', default=2, type=int, help='number of pruning times')
-parser.add_argument('--pruning_ratio', default=0.2, type=float, help='pruning ratio')
-parser.add_argument('--rewinding_epoch', default=1, type=int, help='rewinding epoch')
-parser.add_argument('--save_dir', default='checkpoint', type=str, help='directory to save checkpoints')
+def main():
+    # In[2]: Argument parser
+    parser = argparse.ArgumentParser(description='Post-training pruning: iterative magnitude pruning (IMP)')
+    parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+    parser.add_argument('--epochs', default=2, type=int, help='number of epochs for each training period')
+    parser.add_argument('--device', default='cuda' if torch.cuda.is_available() else 'cpu', type=str, help='device to use')
+    parser.add_argument('--best_acc', default=0, type=float, help='best test accuracy')
+    parser.add_argument('--start_epoch', default=0, type=int, help='start epoch')
+    parser.add_argument('--pruning_times', default=2, type=int, help='number of pruning times')
+    parser.add_argument('--pruning_ratio', default=0.2, type=float, help='pruning ratio')
+    parser.add_argument('--rewinding_epoch', default=1, type=int, help='rewinding epoch')
+    parser.add_argument('--save_dir', default='checkpoint', type=str, help='directory to save checkpoints')
 
-args, unknown = parser.parse_known_args()
+    args, unknown = parser.parse_known_args()
 
-if unknown:  # if there are unrecognized arguments
-    print(f"Warning: Unrecognized arguments: {' '.join(unknown)}", flush=True)
+    if unknown:  # if there are unrecognized arguments
+        print(f"Warning: Unrecognized arguments: {' '.join(unknown)}", flush=True)
 
-# Extracting values from arguments
-lr = args.lr
-epochs = args.epochs
-device = args.device
-best_acc = args.best_acc
-start_epoch = args.start_epoch
-pruning_times = args.pruning_times
-pruning_ratio = args.pruning_ratio
-rewinding_epoch = args.rewinding_epoch
-save_dir = args.save_dir
+    # Extracting values from arguments
+    lr = args.lr
+    epochs = args.epochs
+    device = args.device
+    best_acc = args.best_acc
+    start_epoch = args.start_epoch
+    pruning_times = args.pruning_times
+    pruning_ratio = args.pruning_ratio
+    rewinding_epoch = args.rewinding_epoch
+    save_dir = args.save_dir
 
-# In[3]:
+    # In[3]:
 
-print('# Post-training pruning: iterative magnitude pruning (IMP) #')
+    print('# Post-training pruning: iterative magnitude pruning (IMP) #')
 
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+    # Data
+    print('==> Preparing data..')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2)
 
-# Model
-print('==> Building model..', flush=True)
-net = ResNet18()
-net = net.to(device)
+    # Model
+    print('==> Building model..', flush=True)
+    net = ResNet18()
+    net = net.to(device)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs)
-
-print('# Start Standard Training Iterative Pruning #', flush=True)
-
-for state in range(pruning_times):
-
-    print('******************************************', flush=True)
-    print('pruning state', state, flush=True)
-    print('******************************************', flush=True)
-
-    for epoch in range(start_epoch, start_epoch+epochs):
-        train(epoch, device, net, trainloader, optimizer, criterion)
-        if state == 0:
-            if (epoch+1) == rewinding_epoch:
-                torch.save(net.state_dict(), os.path.join(save_dir, 'epoch_{}_rewind_weight.pt'.format(epoch+1)))
-                rewind_init = deepcopy(net.state_dict())
-        test(epoch, device, net, testloader, criterion, best_acc)
-        scheduler.step()
-    
-    # model pruning and rewinding
-    pruning_model(net, pruning_ratio)
-    current_mask = extract_mask(net.state_dict())
-    remove_prune(net)
-
-    net.load_state_dict(rewind_init, strict=True)
-    prune_model_custom(net, current_mask)
-    
+    criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs)
 
-    for _ in range(rewinding_epoch):
-        scheduler.step()
+    print('# Start Standard Training Iterative Pruning #', flush=True)
 
-    check_sparsity(net)
-    
+    for state in range(pruning_times):
 
-print("Finished!", flush=True)
+        print('******************************************', flush=True)
+        print('pruning state', state, flush=True)
+        print('******************************************', flush=True)
 
+        for epoch in range(start_epoch, start_epoch+epochs):
+            train(epoch, device, net, trainloader, optimizer, criterion)
+            if state == 0:
+                if (epoch+1) == rewinding_epoch:
+                    torch.save(net.state_dict(), os.path.join(save_dir, 'epoch_{}_rewind_weight.pt'.format(epoch+1)))
+                    rewind_init = deepcopy(net.state_dict())
+            test(epoch, device, net, testloader, criterion, best_acc)
+            scheduler.step()
+        
+        # model pruning and rewinding
+        pruning_model(net, pruning_ratio)
+        current_mask = extract_mask(net.state_dict())
+        remove_prune(net)
+
+        net.load_state_dict(rewind_init, strict=True)
+        prune_model_custom(net, current_mask)
+        
+        optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs)
+
+        for _ in range(rewinding_epoch):
+            scheduler.step()
+
+        check_sparsity(net)
+        
+
+    print("Finished!", flush=True)
+
+if __name__ == "__main__":
+    main()
