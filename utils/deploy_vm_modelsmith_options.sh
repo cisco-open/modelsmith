2daf6c152120c10dae22592df9ed4a0dd8879341
Pop John <john.pop@airgap.io>
2024-02-29 12:04:09 +0200

    add resync command without models

--- utils/deploy_vm_modelsmith_options.sh
+++ utils/deploy_vm_modelsmith_options.sh
@@ -23,52 +23,57 @@ cd ../utils
 
 echo -e "${YELLOW}Select the deployment option:${NC}"
 echo "1) Update the entire modelsmith project"
-echo "2) Update bash folder"
-echo "3) Update iterative_magnitude_pruning.py script"
-echo "4) Update block_recon.py"
-echo "5) Update utils.py"
-echo "6) Update pruner.py"
-echo "7) Update basic-ptq-example.py"
-echo "8) Update quantization train.py"
+echo "2) Copy the entire modelsmith project without data, checkpoint, and models_checkpoint folders"
+echo "3) Update bash folder"
+echo "4) Update iterative_magnitude_pruning.py script"
+echo "5) Update block_recon.py"
+echo "6) Update utils.py"
+echo "7) Update pruner.py"
+echo "8) Update basic-ptq-example.py"
+echo "9) Update quantization train.py"
 read -p "Enter your choice: " choice
 
 case $choice in
   1)
     echo -e "${GREEN}Updating the entire modelsmith project... (warning: this will update the entire folder in the VM but preserve data, checkpoint, and models_checkpoint folders)${NC}"
     ssh ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST} "mkdir -p /home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}"
-    rsync -avz --exclude 'data/' --exclude 'checkpoint/' --exclude 'models_checkpoint/' ../modelsmith/ ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}
+    rsync -avz --exclude 'data/' --exclude 'checkpoint/' --exclude 'models_checkpoints/' ../modelsmith/ ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}
     ;;
   2)
+    echo -e "${GREEN}Copying the entire modelsmith project excluding data, checkpoint and models_checkpoint folders...${NC}"
+    rsync -avz --exclude 'data' --exclude 'checkpoint' --exclude 'models_checkpoints' ../modelsmith/ ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}
+    ;;
+  3)
     echo -e "${GREEN}Updating bash folder...${NC}"
     ssh ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST} "find /home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}/bash -maxdepth 1 -type f -delete"
     scp -r ../modelsmith/bash ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}
     ;;
-  3)
+  4)
     echo -e "${GREEN}Updating iterative_magnitude_pruning.py script...${NC}"
     ssh ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST} "rm -f /home/${PRIMARY_SSH_USERNAME}/modelsmith/examples_pruning/iterative_magnitude_pruning.py"
     scp ../modelsmith/examples_pruning/iterative_magnitude_pruning.py ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}/examples
     ;;
-  4)
+  5)
     echo -e "${GREEN}Updating block_recon.py...${NC}"
     ssh ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST} "rm -f /home/${PRIMARY_SSH_USERNAME}/modelsmith/utils/quant/block_recon.py"
     scp ../modelsmith/utils/quant/block_recon.py ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}/utils/quant
     ;;
-  5)
+  6)
     echo -e "${GREEN}Updating utils.py...${NC}"
     ssh ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST} "rm -f /home/${PRIMARY_SSH_USERNAME}/modelsmith/utils/utils.py"
     scp ../modelsmith/utils/utils.py ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}/utils/
     ;;
-  6)
+  7)
     echo -e "${GREEN}Updating pruner.py...${NC}"
     ssh ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST} "rm -f /home/${PRIMARY_SSH_USERNAME}/modelsmith/utils/pruner.py"
     scp ../modelsmith/utils/pruner.py ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}/utils/
     ;;
-  7)
+  8)
     echo -e "${GREEN}Updating basic-ptq-example.py...${NC}"
     ssh ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST} "rm -f /home/${PRIMARY_SSH_USERNAME}/modelsmith/examples_quant/basic-ptq-example.py"
     scp ../modelsmith/examples_quant/basic-ptq-example.py ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}/examples_quant/
     ;;
-  8)
+  9)
     echo -e "${GREEN}Updating quantization train.py...${NC}"
     ssh ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST} "rm -f /home/${PRIMARY_SSH_USERNAME}/modelsmith/examples_quant/train.py"
     scp ../modelsmith/examples_quant/train.py ${PRIMARY_SSH_USERNAME}@${PRIMARY_SSH_HOST}:/home/${PRIMARY_SSH_USERNAME}/${MODELSMITH_PATH}/examples_quant/
