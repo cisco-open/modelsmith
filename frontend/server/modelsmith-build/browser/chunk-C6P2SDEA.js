19e314ebc184f26428df0bfada6ba25b19dca882
Pop John <john.pop@airgap.io>
2024-07-10 14:23:21 +0300

    rebuild app

---
+++ frontend/server/modelsmith-build/browser/chunk-C6P2SDEA.js
@@ -0,0 +1,1 @@
+import{tb as e,ub as r}from"./chunk-DXRB2X5O.js";var o=r({source:"[Core -> Parameter]",events:{"Load Parameters":e(),"Load Parameters Success":e(),"Load Parameters Failure":e()}});export{o as a};
