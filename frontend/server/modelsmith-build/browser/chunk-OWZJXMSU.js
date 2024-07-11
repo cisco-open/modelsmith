157ed8e56a15f570023e51c7182c70373e68ec2d
Pop John <john.pop@airgap.io>
2024-07-11 13:06:27 +0300

    rebuild

---
+++ frontend/server/modelsmith-build/browser/chunk-OWZJXMSU.js
@@ -0,0 +1,1 @@
+import{Ab as r,zb as e}from"./chunk-AIAH5BT5.js";var o=r({source:"[Core -> Parameter]",events:{"Load Parameters":e(),"Load Parameters Success":e(),"Load Parameters Failure":e()}});export{o as a};
--- frontend/server/modelsmith-build/browser/chunk-WL3ZZTTC.js
+++
@@ -1,1 +0,0 @@
-import{Ab as r,zb as e}from"./chunk-JA5VSLUI.js";var o=r({source:"[Core -> Parameter]",events:{"Load Parameters":e(),"Load Parameters Success":e(),"Load Parameters Failure":e()}});export{o as a};
