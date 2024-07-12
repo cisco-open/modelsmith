5fa17a3537e7d47bd18320018056dbc0ccc0e2bd
Pop John <john.pop@airgap.io>
2024-07-12 15:27:56 +0300

    app performance optimization

---
+++ frontend/server/modelsmith-build/browser/chunk-D2Y5ESR4.js
@@ -0,0 +1,1 @@
+import{Kc as e,Lc as r}from"./chunk-NPZHNGVX.js";var o=r({source:"[Core -> Parameter]",events:{"Load Parameters":e(),"Load Parameters Success":e(),"Load Parameters Failure":e()}});export{o as a};
--- frontend/server/modelsmith-build/browser/chunk-OWZJXMSU.js
+++
@@ -1,1 +0,0 @@
-import{Ab as r,zb as e}from"./chunk-AIAH5BT5.js";var o=r({source:"[Core -> Parameter]",events:{"Load Parameters":e(),"Load Parameters Success":e(),"Load Parameters Failure":e()}});export{o as a};
