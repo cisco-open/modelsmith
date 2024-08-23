c5deadb7b5c4cce25cb567eea804c3d051beff09
Pop John <john.pop@airgap.io>
2024-08-23 14:00:10 +0300

    deploy

---
+++ frontend/server/modelsmith-build/browser/chunk-VLJTKNYA.js
@@ -0,0 +1,1 @@
+import{Hc as e,Ic as r}from"./chunk-64NP6AY6.js";var o=r({source:"[Core -> Parameter]",events:{"Load Parameters":e(),"Load Parameters Success":e(),"Load Parameters Failure":e()}});export{o as a};
