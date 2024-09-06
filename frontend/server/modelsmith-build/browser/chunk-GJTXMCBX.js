1eb1ee5755ec35af72b663dcecd324bd53da3759
Pop John <john.pop@airgap.io>
2024-09-06 14:31:03 +0300

    rebuild app

---
+++ frontend/server/modelsmith-build/browser/chunk-GJTXMCBX.js
@@ -0,0 +1,1 @@
+import{Hc as o,Ic as r}from"./chunk-64NP6AY6.js";var i=r({source:"[Core -> Configs]",events:{"Set Default Mode":o(),"Set Current Mode":o()}});var t=function(e){return e.GUIDED="guided",e.EXPERT="expert",e}(t||{});export{i as a,t as b};
