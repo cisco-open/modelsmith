39f97a3625e42f4ae9e9b483616f2a02024f24e2
Pop John <john.pop@airgap.io>
2024-10-22 12:13:44 +0300

    disable the clear command if script is running and rebuild the app

--- frontend/server/modelsmith-build/browser/chunk-3JUE3TO2.js
+++
@@ -1,1 +0,0 @@
-import{Gc as e,Hc as r}from"./chunk-CQN7FGIC.js";var o=r({source:"[Core -> Parameter]",events:{"Load Parameters":e(),"Load Parameters Success":e(),"Load Parameters Failure":e()}});export{o as a};
---
+++ frontend/server/modelsmith-build/browser/chunk-BTI2I76F.js
@@ -0,0 +1,1 @@
+import{Gc as e,Hc as r}from"./chunk-DL2LTWBU.js";var o=r({source:"[Core -> Parameter]",events:{"Load Parameters":e(),"Load Parameters Success":e(),"Load Parameters Failure":e()}});export{o as a};
