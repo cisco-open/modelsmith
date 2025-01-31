bddd6563cb8edb6561a7aa6eca6ffb30a696d0df
Pop John <john.pop@airgap.io>
2025-01-31 11:45:59 +0200

    rebuild app

--- frontend/server/modelsmith-build/browser/chunk-NIBPW2FQ.js
+++
@@ -1,1 +0,0 @@
-import{Kc as e,Lc as r}from"./chunk-2CPYVQID.js";var o=r({source:"[Core -> Parameter]",events:{"Load Parameters":e(),"Load Parameters Success":e(),"Load Parameters Failure":e()}});export{o as a};
